9091606ed0e33acaecaa946ba694fe08cb856d6a
Urvisha Jain <en19cs301396@medicaps.ac.in>
2021-11-12 10:59:57 +0530

    django added

---
+++ home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse
+def main(request):
+    
+    return render(request,'home/main.html')
+def signups(request):
+    return render(request,'home/signup Students.html')
+def signupr(request):
+    return render(request,'home/sign up Recruiters.html')
+def contact(request):
+    return render(request,'home/portal.html')
+def credits(request):
+    return render(request,'home/credits.html')
+
+
+# Create your views here.
